2b74fe5375e884cf18320f6595d7e2ff5fe4c29c
Bhavishya <bhavishya2107@gmail.com>
2019-12-14 02:32:31 +0530

    add icons/images for day night cities add dark mode for night

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,11 @@
 const cityForm = document.querySelector('form')
 const card = document.querySelector('.card')
 const details = document.querySelector('.details')
+const time = document.querySelector('img.time')
+const icon = document.querySelector('.icon img')
+const day = document.querySelector('.day')
+const body = document.querySelector('body')
+const label = document.querySelector('label')
 
 const updateUI = (data) => {
 
@@ -14,6 +19,26 @@ const updateUI = (data) => {
                         <span>&deg;C</span>
                         </div>`;
     
+    //update the night/day images
+    const iconSrc = `assets/img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src',iconSrc)
+
+
+    let timeSrc = null
+    if(weather.IsDayTime){
+        timeSrc = 'assets/img/day.svg'
+        day.textContent = "GOOD DAY"
+        day.style.color = "darkgrey"
+        day.style.fontweight = "100"
+    } else {
+        timeSrc = 'assets/img/night.svg'
+        day.textContent = "Good Evening"
+        body.style.background = "black"
+        label.style.color = "white"
+    }
+    time.setAttribute('src', timeSrc);
+
+
     //remove d-none class
     if(card.classList.contains('d-none')){
         card.classList.remove('d-none')
